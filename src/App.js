a9398d7e021fc0ff3dd31465480077f89c6ca67c
KavindranM <119048365+KavindranM@users.noreply.github.com>
2022-11-25 16:41:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import RegisterForm from './components/form';
+//import './Vform.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm/>
+    </div>
+  );
+}
+
+export default App;
